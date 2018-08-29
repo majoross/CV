20407b5ddac124da0e968385c3ffed26733743b1
Majoross Dániel <majoross.d@gmail.com>
2018-08-29 13:50:46 +0200

    cv one pager

---
+++ CV/src/main/java/com/cv/CvApplication.java
@@ -0,0 +1,12 @@
+package com.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvApplication.class, args);
+	}
+}
